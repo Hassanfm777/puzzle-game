2eff7c9f9edebb51f5544732ad0df8780da872e3
Hassanfm777 <starspoly1@hotmail.com>
2025-05-01 12:16:41 +0400

    JS

--- script.js
+++ script.js
@@ -107,24 +107,3 @@ function swapTiles(index1, index2) {
   tiles[index1].setAttribute('data-index', tiles[index2].getAttribute('data-index'));
   tiles[index2].setAttribute('data-index', tempIndex);
 }
-
-function generateJigsawClipPath(col, row) {
-  const indent = 10;
-  let clipPath = '';
-  const isLeftEdge = col === 0;
-  const isRightEdge = col === Math.sqrt(size) - 1;
-  const isTopEdge = row === 0;
-  const isBottomEdge = row === Math.sqrt(size) - 1;
-
-  if (isLeftEdge) {
-    clipPath += `polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)`;
-  } else if (isRightEdge) {
-    clipPath += `polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)`;
-  } else if (isTopEdge) {
-    clipPath += `polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)`;
-  } else {
-    clipPath += `polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)`;
-  }
-
-  return clipPath;
-}
