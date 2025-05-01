1d07b1d2662ed5090f247b52cebdc63ec4ed49bc
Hassanfm777 <starspoly1@hotmail.com>
2025-05-01 10:33:28 +0400

    js v2

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function moveTile(index) {
     if (targetIndex >= 0 && targetIndex < size * size) {
       const row = Math.floor(targetIndex / size);
       const col = targetIndex % size;
-      
+
       if ((offset === -1 || offset === 1) && Math.floor(index / size) === row) {
         if (targetIndex === emptyIndex) {
           swapTiles(index, emptyIndex);
@@ -59,11 +59,21 @@ function moveTile(index) {
 }
 
 function swapTiles(index1, index2) {
-  [tiles[index1].style.backgroundImage, tiles[index2].style.backgroundImage] = 
-  [tiles[index2].style.backgroundImage, tiles[index1].style.backgroundImage];
-  
+  const tempBg = tiles[index1].style.backgroundImage;
+  const tempPos = tiles[index1].style.backgroundPosition;
+
+  tiles[index1].style.backgroundImage = tiles[index2].style.backgroundImage;
+  tiles[index1].style.backgroundPosition = tiles[index2].style.backgroundPosition;
+
+  tiles[index2].style.backgroundImage = tempBg;
+  tiles[index2].style.backgroundPosition = tempPos;
+
   tiles[index1].classList.toggle('empty');
   tiles[index2].classList.toggle('empty');
+
+  const tempIndex = tiles[index1].getAttribute('data-index');
+  tiles[index1].setAttribute('data-index', tiles[index2].getAttribute('data-index'));
+  tiles[index2].setAttribute('data-index', tempIndex);
 }
 
 createPuzzle();
